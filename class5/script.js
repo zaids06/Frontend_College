290e5864bbb09aae0aaf03354649a844d7e015c7
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2024-01-29 11:16:07 +0530

    Class 5

---
+++ class5/script.js
@@ -0,0 +1,30 @@
+// var abfc=4;
+// var abfc=10;
+// abfc=5;
+// console.log(abfc);
+// let efg=6;
+// efg=6;
+// console.log(efg);
+// const ab=55;
+
+// let ob={
+//   name:"Zaid",
+//   section:"K22MZ",
+// };
+// console.log(ob.name);
+
+
+// for (let i =0;i<5;++i)
+//     console.log(i);
+
+let a=4;
+let b=5;
+console.log(a+b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+console.log(a-b);
+console.log(a++);
+console.log(a--);
+console.log(++a);
+console.log(--a);
---
+++ taskClass5/script.js
@@ -0,0 +1,8 @@
+let price1=10;
+let price2=20;
+let price3=30;
+// let priceIndividual=price3/2;
+let totalBeforeDiscount=(price1+price2+(price3*2));
+console.log("Total before discount = "+totalBeforeDiscount);
+console.log("Average price of item before discount = "+totalBeforeDiscount/4);
+console.log("Total price after discount = "+ (totalBeforeDiscount-5));
