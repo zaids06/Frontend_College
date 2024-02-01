51adfe6f00a6388d47eabc0abd3e1ff3d5d2fe2f
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2024-02-01 12:08:08 +0530

    Tasks for class 6

---
+++ class6/task 4/script.js
@@ -0,0 +1,14 @@
+// let docQuery=(str)=>document.querySelector(str);
+// let questionStatement=docQuery('.question');
+// questionStatement.textContent=' this is added';
+
+// let res=0;
+// for (let i=1;i<=20;i++)
+// {
+//     res+=i;
+//     console.log(`Sum of first ${i} numbers = ${res}`);
+// }
+
+for (let i =51;i<=151;i++)
+    if(!((i%2)==0))
+        console.log(i);
