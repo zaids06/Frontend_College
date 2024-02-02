e9b677bd6f295aae648a6ee1ba14de0983cf7893
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2024-02-02 22:55:04 +0530

    Tasks for class 7

---
+++ class7/task4/script.js
@@ -0,0 +1,10 @@
+let btnSubmit=document.getElementById('submit')
+let nameField=document.getElementById('name');
+let pass=document.getElementById('pass');
+let email=document.getElementById('email');
+let validate=function (){
+    if(nameField.textContent==='');
+    alert('Please enter your name');
+}
+
+btnSubmit.addEventListener('click',validate)
