b240be5bf83b2c11342b972b7b2183bad04edba4
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2024-02-05 23:17:41 +0530

    Project

--- class7/task4/script.js
+++ class7/task4/script.js
@@ -2,6 +2,7 @@ let btnSubmit=document.getElementById('submit')
 let nameField=document.getElementById('name');
 let pass=document.getElementById('pass');
 let email=document.getElementById('email');
+
 let validate=function (){
     if(nameField.textContent==='');
     alert('Please enter your name');
