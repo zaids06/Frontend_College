066394ff9ff60741117bc3619e2ba332cc9c3796
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2024-02-10 23:50:35 +0530

    Project

--- class7/task4/script.js
+++ class7/task4/script.js
@@ -1,6 +1,7 @@
 let btnSubmit=document.getElementById('submit')
 let nameField=document.getElementById('name');
 let pass=document.getElementById('pass');
+
 let email=document.getElementById('email');
 
 
